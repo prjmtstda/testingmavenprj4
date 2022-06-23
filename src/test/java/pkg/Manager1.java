fe12bc64f161aed781fa6aea68a31a6a832d695d
QCT <snkta2018@gmail.com>
2022-06-23 15:29:24 +0530

    Commit.

--- src/test/java/pkg/Manager1.java
+++ src/test/java/pkg/Manager1.java
@@ -7,5 +7,7 @@ public class Manager1
 		System.out.println("QC Testing");
 		System.out.println("b1 - QC Testing");
 		System.out.println("Local - b1 - QC Testing");
+		System.out.println("Testing");
+		System.out.println("Testing");
 	}
 }
