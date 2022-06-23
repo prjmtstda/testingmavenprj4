3c6671eb138248bbe0d88efe8791c6b969f07cd6
QCT <snkta2018@gmail.com>
2022-06-23 14:53:54 +0530

    Commit.

---
+++ src/test/java/pkg/Manager1.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class Manager1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Testing");
+		System.out.println("QC Testing");
+		System.out.println("Done");
+	}
+}
